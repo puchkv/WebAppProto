5e9cd651377a281dc2ce16fac68554fca71b697a
Eugene <pucha1998@gmail.com>
2023-09-25 10:42:40 +0300

    THEME UPDATE

--- theme.js
+++ theme.js
@@ -1,45 +1,17 @@
 
-
-
 class Theme {
-
-    constructor() {
-        const colors = window.Telegram.WebApp.themeParams;
-    }
 
     init() {
 
         const icons = document.querySelectorAll("object[type='image/svg+xml']");
 
+        const primaryColor = window.Telegram.WebApp.MainButton.color ?? "#3478F6";
+
         icons.forEach(icon => {
-
             icon.addEventListener("load", function() {
-                icon.contentDocument.querySelector("svg").style.stroke = "red";
+                icon.contentDocument.querySelector("svg").style.stroke = primaryColor;
             });
-
-            
         });
-
-        this.#apply();
-    }
-
-
-    #apply() {
-
-        if(this.colors === undefined) {
-            return;
-        }
-
-        // if(this.colors.bg_color !== undefined) {
-        //     document.querySelector("body").style.background = this.colors.bg_color;
-        // }
-
-        // if(this.colors.text_color !== undefined) {
-        //     document.querySelector("body").style.color = this.colors.text_color;
-        // }
-
-        
-
     }
 
 }
